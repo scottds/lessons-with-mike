f3d6fae089eacc29bcb1d91f0de819818d25de1e
scottds <scottshattuckmedia@gmail.com>
2017-05-03 16:57:07 -0600

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+"use strict";
+angular.module('myApp',[]);
+angular.module('myApp').controller('MainController', ['$scope', '$interval', function($scope, $interval){
+    var items = ['bananas','apples','pears','cherries','peaches'];
+    $scope.itemIndex = null;
+    $scope.currentItem = '';
+    $scope.getItem = function(){
+        $scope.currentItem = items[$scope.itemIndex];
+    };
+    $interval(function() {
+        $scope.itemIndex = Math.round(Math.random() * (items.length - 1));
+        $scope.getItem();
+    }, 2000);
+}]);
